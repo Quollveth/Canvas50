d6833733e0c248aa30b17cbe32b172e34a72d1b1
quollveth <klaus.marrer@outlook.com>
2024-04-06 13:45:11 -0300

    carousel generator

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -9,12 +9,15 @@ let savedCanvas = JSON.parse(localStorage.getItem(canvasStorageName));
 
 function addCarouselItem(){
     let newItem = document.createElement('div');
-    newItem.id = carouselWrapper.children.length;
-    newItem.classList.add('item');
+    newItem.id = `ci-${carouselWrapper.children.length}`;
+    newItem.classList.add('carousel-item');
     let itemBody;
 
     itemBody = document.createElement('img');
-    itemBody.src = 'https://via.placeholder.com/200';
+    itemBody.src = 'https://via.placeholder.com/500';
+    let temp = document.createElement('p');
+    temp.innerText = newItem.id;
+    newItem.appendChild(temp);
 
     newItem.appendChild(itemBody);
     
@@ -22,7 +25,7 @@ function addCarouselItem(){
     indicator.setAttribute('data-target','#galleryCarousel');
     indicator.setAttribute('data-slide-to',newItem.id);
 
-    if(newItem.id == 0){
+    if(newItem.id == 'ci-0'){
         newItem.classList.add('active');
         indicator.classList.add('active');
     }
